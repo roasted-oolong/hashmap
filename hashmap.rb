30b75ef86e7fdde144fd227257f58ed1bd9831ba
Jenna Lee <65867683+roasted-oolong@users.noreply.github.com>
2025-02-10 17:29:24 +0000

    Complete hash method

--- hashmap.rb
+++ hashmap.rb
@@ -5,7 +5,12 @@ class HashMap
   capacity = 0
 
   def hash(key)
-    #takes a key and produces a hash code with it
+    hash_code = 0
+    prime_number = 31
+      
+    key.each_char { |char| hash_code = prime_number * hash_code + char.ord }
+      
+    hash_code
   end
 
   def set(key, value)
